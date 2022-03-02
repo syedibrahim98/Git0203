ce1a923f736fa8e12986fa3b1cea30b820660518
syedibrahim98 <masyedibrahim449@gmail.com>
2022-03-02 15:18:50 +0530

    Git

---
+++ src/org/test/GitSample.java
@@ -0,0 +1,19 @@
+package org.test;
+
+public class GitSample {
+	
+	public void gitOne() {
+		System.out.println("gitone test1");
+	}
+	
+	public void gitTwo() {
+		System.out.println("gittwo test2");
+	}
+	
+	public static void main(String[] args) {
+		GitSample sample = new GitSample();
+		sample.gitOne();
+		sample.gitTwo();
+	}
+
+}
